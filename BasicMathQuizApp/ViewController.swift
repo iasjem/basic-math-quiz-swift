c69c569511744e1ccea03b01fe5b6026397eda7f
iasjem <jemimah.beryl.sai@gmail.com>
2018-06-23 11:42:19 +0800

    ViewController - Added an Operation enum with functions symbol() and calculate()

--- BasicMathQuizApp/ViewController.swift
+++ BasicMathQuizApp/ViewController.swift
@@ -3,6 +3,38 @@ import Foundation
 // MARK - Enums
 
 // MARK - Functions
+enum Operation {
+    case Add
+    case Subtract
+    case Multiply
+    case Divide
+    
+    func symbol() -> String {
+        switch(self) {
+            case .Add:
+                return "+"
+            case .Subtract:
+                return "-"
+            case .Multiply:
+                return "*"
+            case .Divide:
+                return "/"
+        }
+    }
+
+    func calculate(firstNumber: Int, secondNumber: Int) -> Int {
+         switch(self) {
+            case .Add:
+               return firstNumber + secondNumber
+            case .Subtract:
+                return firstNumber - secondNumber
+            case .Multiply:
+                return firstNumber * secondNumber
+            case .Divide:
+                return firstNumber / secondNumber
+        }
+    }
+}
 
 // MARK - Initial set-up environment
 print("Test")
